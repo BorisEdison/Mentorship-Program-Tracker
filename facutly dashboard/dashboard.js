f9ab6fd06c2dcaba715823a069fbe75f00adf04c
Boris <borisedison@gmail.com>
2022-03-18 18:24:09 +0530

    just started working on faculty dashboard

---
+++ facutly dashboard/dashboard.js
@@ -0,0 +1,9 @@
+// sidebar
+(function () {
+    'use strict'
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+      new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  })()
+  
