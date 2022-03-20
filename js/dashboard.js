7b5ea29e7c6f541bcaec2e4b0e7089ac663afea2
Boris <borisedison@gmail.com>
2022-03-20 16:45:06 +0530

    kebab menu

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,3 +6,15 @@
       new bootstrap.Tooltip(tooltipTriggerEl)
     })
   })()
+
+// kebab menu
+var kebab = document.querySelector('.kebab'),
+middle = document.querySelector('.kebab-middle'),
+cross = document.querySelector('.kebab-cross'),
+dropdown = document.querySelector('.kebab-dropdown');
+
+kebab.addEventListener('click', function() {
+middle.classList.toggle('active');
+cross.classList.toggle('active');
+dropdown.classList.toggle('active');
+})
