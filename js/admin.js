38293115d0d47288f6f9e6187578cf052e4970af
Boris <borisedison@gmail.com>
2022-03-26 20:45:48 +0530

    working on admin page

---
+++ js/admin.js
@@ -0,0 +1,9 @@
+// search
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
