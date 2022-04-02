d03c997b4388d9b3489350386988b3e7f1a863fc
Hrishikesh Panigrahi <hrishikesh.panigrahi38@gmail.com>
2022-04-02 22:44:06 +0530

    docs: few changes

--- MPT/AdminPage/apps.py
+++ MPT/AdminPage/apps.py
@@ -1,4 +1,8 @@
 from django.apps import AppConfig
+from django.contrib.admin.apps import AdminConfig
+
+# class AdminConfig(AdminConfig):
+#     default_site= 'AdminPage.admin.Admin'
 
 
 class AdminpageConfig(AppConfig):
