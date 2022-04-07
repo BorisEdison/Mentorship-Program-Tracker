f75a2d727f79de731d0b56258bba5c020c5e96f7
Boris Edison <borisedison@gmail.com>
2022-04-07 18:57:05 +0530

    sidebar and select buttons

--- MPT/AdminPage/urls.py
+++ MPT/AdminPage/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import EditView
+
 
 urlpatterns = [
     path('AdminPage', views.Adminpage, name= 'admin'),
--- MPT/AdminPage/views.py
+++ MPT/AdminPage/views.py
@@ -2,6 +2,7 @@ from audioop import reverse
 import genericpath
 from sre_constants import SUCCESS
 from typing import Generic
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -23,6 +24,7 @@ def student(request):
     return render(request, 'student-dashboard.html')
 
 def faculty(request):
+
     return render(request, 'faculty-dashboard.html')
     
 
@@ -31,18 +33,41 @@ def studentdetail(request, pk):
     context = {'users': user}
     return render(request, 'student-profile.html', context)
 
-class EditView(generic.UpdateView):
-    model = User
-    form_class = UserChangeForm
-    template_name = 'edit.html'
-    success_url = reverse_lazy('LoginPage')
+# class EditView(generic.UpdateView):
+#     model = User
+#     form_class = UserChangeForm
+#     template_name = 'edit.html'
+#     success_url = reverse_lazy('LoginPage')
 
-    def get_values(self):
-        return self.request.User
+#     def get_values(self):
+#         return self.request.User
+
+# def edit()
 
 
 def edit(request):
-    return render(request,'edit.html')
+    # user=User.objects.get(request.user.id)
+    context={}
+    # user_id = User.request.user.id
+
+    if request.method=="POST":
+        fName = request.POST['fName']
+        # Lname= request.POST['Lname']
+        username= request.POST['username']
+        # department= request.POST['department']
+        # phone= request.POST['phone']
+        # email= request.POST['email']
+        # email1= request.POST['email1']
+        # password1= request.POST['password1']
+        # password2= request.POST['password2']
+        user = User.objects.get(id=request.user.id)
+        user.first_name= fName
+        user.save()
+        print(" gg")
+        return render(request, 'login-page.html')
+
+    else:
+        return render(request,'edit.html')
 
 def stud_prof(request):
     return render(request, 'student-profile.html')
