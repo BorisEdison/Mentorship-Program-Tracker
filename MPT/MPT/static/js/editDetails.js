71d8d13e1997adc8d71a2fa11c51df6d404612cd
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2022-06-11 20:09:14 +0530

    Updated marks tab static template

--- MPT/MPT/static/js/editDetails.js
+++ MPT/MPT/static/js/editDetails.js
@@ -23,80 +23,69 @@ editBody.forEach((elem) => {
 
 // ------------------------  Marks Dynamic Fields ------------------------- //
 let addSemDetail = document.getElementById("add-marks-btn");
-let marksTab = addSemDetail.parentNode;
+let marksTab = document.getElementById("marks").childNodes[3];
+console.log(marksTab);
+//  newtab goes into event listener
+let newtab =
+  '<div class="marks-detail">\
+                <div class="row pt-2">\
+                  <div class="col-6">\
+                    <div class="form-group col-md-10">\
+                      <label for="sem">Semester</label> <br>\
+                      <select id="sem" style="height: 36px;" name="Semester">\
+                        <option value="-">--</option>\
+                        <option value="1">1</option>\
+                        <option value="2">2</option>\
+                        <option value="3">3</option>\
+                        <option value="4">4</option>\
+                        <option value="5">5</option>\
+                        <option value="6">6</option>\
+                        <option value="7">7</option>\
+                        <option value="8">8</option>\
+                      </select>\
+                    </div>\
+                  </div>\
+                  <div class="col-6">\
+                    <div class="form-group col-md-10">\
+                      <label for="gpa">GPA</label>\
+                      <input type="number" step="any" name="gpa" class="form-control" id="gpa" placeholder="GPA" value="0.0">\
+                    </div>\
+                  </div>\
+                </div>\
+                <!--  Add logic to detect number of subjects -->\
+                <div class="row pt-2">\
+                  <div class="col-12">\
+                    <div class="form-group col-md-10">\
+                      <label for="subject">Subject</label> <br>\
+                      <select id="subject" style="height: 36px;" name="Subject">\
+                        <option value="-">--</option>\
+                        <option value="1">Sub name 1</option>\
+                        <option value="2">Sub name 2</option>\
+                        <option value="...">...</option>\
+                      </select>\
+                    </div>\
+                  </div>\
+                </div>\
+                <div class="row pt-2 pb-4">\
+                  <div class="col-4">\
+                    <div class="form-group col-md-10">\
+                      <input type="number" name="IA1" class="form-control" id="IA1" placeholder="IA1 marks" value="0">\
+                    </div>\
+                  </div>\
+                  <div class="col-4">\
+                    <div class="form-group col-md-10">\
+                      <input type="number" name="IA2" class="form-control" id="IA2" placeholder="IA2 marks" value="0">\
+                    </div>\
+                  </div>\
+                  <div class="col-4">\
+                    <div class="form-group col-md-10">\
+                      <input type="number" name="ESE" class="form-control" id="ESE" placeholder="ESE marks" value="0">\
+                    </div>\
+                  </div>\
+                </div> \
+              </div>';
 
 addSemDetail.addEventListener("click", () => {
-  // Semester
-  alert("You unlocked an easter egg")
+  alert("You unlocked an easter egg");
+  marksTab.innerHTML += newtab;
 });
-
-{
-  //  Logic goes into event listener
-  /*  
-    <div class="marks-detail">
-      <div class="row pt-2">
-        <div class="col-6">
-          <div class="form-group col-md-10">
-            <label for="sem">Semester</label> <br>
-            <select id="sem" style="height: 36px;" name="Semester">
-              <option value="-">--</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="col-6">
-          <div class="form-group col-md-10">
-            <label for="gpa">GPA</label>
-            <input type="number" step="any" name='gpa' class="form-control" id="gpa"
-              placeholder="GPA" value='0.0'>
-          </div>
-        </div>
-      </div> 
-      / --------- Add logic to detect number of subjects ------------ 
-      <div class="row pt-2">
-        <div class="col-12">
-          <div class="form-group col-md-10">
-            <label for="subject">Subject</label> <br>
-            <select id="subject" style="height: 36px;" name="Subject">
-              <option value="-">--</option>
-              <option value="1">Sub name 1</option>
-              <option value="2">Sub name 2</option>
-              <option value="3">Sub name 3</option>
-              <option value="4">Sub name 4</option>
-              <option value="5">Sub name 5</option>
-            </select>
-          </div>
-        </div>
-      </div>
-
-      <div class="row pt-2">
-        <div class="col-4">
-          <div class="form-group col-md-10">
-            <input type="number" name='IA1' class="form-control" id="IA1"
-              placeholder="IA1 marks" value='0'>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="form-group col-md-10">
-            <input type="number" name='IA2' class="form-control" id="IA2"
-              placeholder="IA2 marks" value='0'>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="form-group col-md-10">
-            <input type="number" name='ESE' class="form-control" id="ESE"
-              placeholder="ESE marks" value='0'>
-          </div>
-        </div>
-      </div> 
-    </div>
-  */
-}
