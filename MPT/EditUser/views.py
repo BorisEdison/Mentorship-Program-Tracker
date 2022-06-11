8674014e4c71af76ff6d4feb62e1d08d9a92d61d
Bipin_Giri <bipingiri0085@gmail.com>
2022-06-11 15:35:36 +0530

    deleting previous image if new image is uploaded

--- MPT/EditUser/views.py
+++ MPT/EditUser/views.py
@@ -8,8 +8,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 # from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
-from accounts.models import StudentProfile
-from accounts.models import User
+from accounts.models import StudentProfile,User
 
 
 #  need to do 1-1 relationship with user and mentor while registration 
@@ -20,7 +19,6 @@ def edit(request):
             if request.method=="POST":
                 fName = request.POST['fName']
                 Lname= request.POST['LName']
-                username= request.POST['username']
                 department= request.POST['department']
                 # phone= request.POST['phone']
                 # email= request.POST['email']
@@ -29,8 +27,9 @@ def edit(request):
                 # password2= request.POST['password2']
                 profile_img= request.FILES['profile_img']
                 user = User.objects.get(id=request.user.id)
+                if str(user.profile_img) != 'logo.png': # if user already has profile image then delete it
+                    user.profile_img.delete()
                 user.first_name= fName
-                user.username=username
                 user.profile_img=profile_img
                 user.last_name=Lname
                 user.save()
