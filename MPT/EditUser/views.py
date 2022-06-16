8d1f6f968e8dcaac5b63bb29539abc29d7918b73
Bipin_Giri <bipingiri0085@gmail.com>
2022-06-17 04:33:26 +0530

    edit page working for staff

--- MPT/EditUser/views.py
+++ MPT/EditUser/views.py
@@ -3,71 +3,95 @@ import genericpath
 from sre_constants import SUCCESS
 from typing import Generic
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 # from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserChangeForm
-from accounts.models import StudentProfile,User, MentorProfile
+from accounts.models import StudentProfile, User, MentorProfile
 
 
-# need to do 1-1 relationship with user and mentor while registration 
+# need to do 1-1 relationship with user and mentor while registration
 @login_required
 def edit(request):
-    context={}
+    context = {}
     # print(request.user)
     # print(request.user.id)
+
     if request.user.is_staff:
-            profile = MentorProfile.objects.get(user__id=request.user.id)
-            print("mentor id",profile.id)
-            print("user id",profile.user.id)
+        profile = MentorProfile.objects.get(user__id=request.user.id)
 
-            print(profile.Branch)
+        # print("mentor id", profile.id)
+        # print("user id", profile.user.id)
+        # print(profile.Branch)
 
-            context={   'profile': profile,
-                        'id': request.user.id}
+        context = {
+            'department_list': [
+                'Computer Engineering',
+                'Electronics and Telecommunication Engineering',
+                'Information Technology',
+                'Mechanical Engineering'
+            ],
+            'bloodGroup_list': [
+                'A+',
+                'A-',
+                'B+',
+                'B-',
+                'AB+',
+                'AB-',
+                'O+',
+                'O-'
+            ],
+            'profile': profile,
+            'id': request.user.id
+        }
 
-            if request.method=="POST":
-                print("user id after post",profile.user.id)
-                print("mentor id after post",profile.id)
+        if request.method == "POST":
+            print("user id after post", profile.user.id)
+            print("mentor id after post", profile.id)
 
-                fName = request.POST['fName']
-                Lname= request.POST['lName']
-                department= request.POST['dept']
-                motherTongue= request.POST['mTongue']
-                religion= request.POST['Religion']
-                # phone= request.POST['phone']
-                # email= request.POST['email']
-                # email1= request.POST['email1']
-                # password1= request.POST['password1']
-                # password2= request.POST['password2']
-                city = request.POST['city']
-                profile_img= request.FILES['profileImg']
-                user = User.objects.get(id=request.user.id)
-                if str(user.profile_img) != 'logo.png': # if user already has profile image then delete it
+            fName = request.POST['fName']
+            Lname = request.POST['lName']
+            department = request.POST['dept']
+            motherTongue = request.POST['mTongue']
+            religion = request.POST['Religion']
+            phone = request.POST['phone']
+            city = request.POST['city']
+            blood_group = request.POST['blood_group']
+            gender = request.POST['gender']
+            DateofBirth = request.POST['dob']
+            user = User.objects.get(id=request.user.id)
+
+            if 'profileImg' in request.FILES:
+                profile_img = request.FILES['profileImg']
+                # if user already has profile image then delete it
+                if str(user.profile_img) != 'logo.png':
                     user.profile_img.delete()
-                user.first_name= fName
-                user.profile_img=profile_img
-                user.last_name=Lname
-                user.save()
+                user.profile_img = profile_img
 
-                profile.city = city
-                profile.Branch = department
-                profile.mother_tongue = motherTongue
-                profile.religion = religion
-                # print(profile.city)
-                profile.save()
-                # print("hello1")
-                return redirect('faculty', pk = user.id)
-            else:
-                return render(request,'faculty-edit.html', context)
+            user.first_name = fName
+            user.last_name = Lname
+            user.phone = phone
+            user.save()
+
+            profile.city = city
+            profile.Blood_grp = blood_group
+            profile.Branch = department
+            profile.DateofBirth = DateofBirth
+            profile.Gender = gender
+            profile.mother_tongue = motherTongue
+            profile.religion = religion
+            profile.save()
+            return redirect('faculty', pk=user.id)
+        else:
+            return render(request, 'faculty-edit.html', context)
 
     else:
         profile = StudentProfile.objects.get(user__id=request.user.id)
-        context={'profile': profile}
+        context = {'profile': profile}
         print(request.user)
-        if request.method=="POST":
+        if request.method == "POST":
             fName = request.POST['fName']
             Lname = request.POST['LName']
             department = request.POST['department']
@@ -76,20 +100,20 @@ def edit(request):
             # email1= request.POST['email1']
             # password1= request.POST['password1']
             # password2= request.POST['password2']
-            profile_img= request.FILES['profile_img']
+            profile_img = request.FILES['profile_img']
             user = User.objects.get(id=request.user.id)
-            if str(user.profile_img) != 'logo.png': # if user already has profile image then delete it
+            # if user already has profile image then delete it
+            if str(user.profile_img) != 'logo.png':
                 user.profile_img.delete()
-            user.first_name= fName
-            user.last_name=Lname
-            user.profile_img=profile_img
+            user.first_name = fName
+            user.last_name = Lname
+            user.profile_img = profile_img
             user.save()
 
-            profile.department= department
+            profile.department = department
             profile.save()
-            
+
             return redirect('/studentdashboard')
 
-
         else:
-            return render(request,'edit-student-details.html', context)
+            return render(request, 'edit-student-details.html', context)
--- MPT/FacultyDashboard/urls.py
+++ MPT/FacultyDashboard/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('facultydashboard/<str:pk>', views.faculty, name= 'faculty'),
     path('facultydashboard/student/<str:pk>/', views.studentdetail, name= 'studentdetail'),
     path('logout/', views.logout, name= 'logout'),
-    # path('/', include('EditUser.urls')),    
 
 ]
