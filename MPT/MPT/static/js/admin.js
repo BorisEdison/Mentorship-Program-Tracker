e347285a2fe4a27431e0b23c563ded2f41ef1864
05Alston <0508alstonfernandes@gmail.com>
2022-06-18 13:23:45 +0530

    added basic datatable template

--- MPT/MPT/static/js/admin.js
+++ MPT/MPT/static/js/admin.js
@@ -1,9 +1,25 @@
 // search
-$(document).ready(function(){
-    $("#myInput").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#myTable tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-    });
-  });
+// $(document).ready(function(){
+//     $("#myInput").on("keyup", function() {
+//       var value = $(this).val().toLowerCase();
+//       $("#myTable tr").filter(function() {
+//         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+//       });
+//     });
+//   });
+
+
+
+// ---------- Adding Datatables ------------------
+let usr_table = new DataTable('#user-table', {
+    // options
+});
+let studnt_table = new DataTable('#student-table', {
+    // options
+});
+let mntr_table = new DataTable('#mentor-table', {
+    // options
+});
+let actvt_table = new DataTable('#activity-table', {
+    // options
+});
