6c7121478bcc3c2f6341d6ed88e853f99400284c
Bipin_Giri <bipingiri0085@gmail.com>
2022-06-23 20:24:56 +0530

    rendering assigned mentee details on  faculty/mentor dashboard

--- MPT/FacultyDashboard/views.py
+++ MPT/FacultyDashboard/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserChangeForm
-
-from accounts.models import StudentProfile, StudentDetails, MentorProfile, Mentor_assign
+from accounts.models import StudentProfile, User, MentorProfile, Mentor_assign, StudentDetails, StudentHobbies,GuardianDetails,StudentExtraCurricular,StudentMedicalReport
 from django.contrib.auth.decorators import permission_required
 from accounts.models import User
 from django.contrib.auth import logout as django_logout
@@ -22,20 +21,21 @@ def logout(request):
 def studentdetail(request, fac_id, stu_id):
     user = User.objects.get(usr_id=stu_id)
     faculty = User.objects.get(usr_id=fac_id)
-    # user = StudentDetails.objects.get(student__user__usr_id = request.user.usr_id)
-    # user = User.objects.get(usr_id=pk)
-    # print (request.user.usr_id)
-    # print(user.usr_id)
-    studentProfile = StudentProfile.objects.get(user__usr_id = stu_id)
-    # print(studentProfile.user.usr_id)
-    # details = StudentDetails.objects.get(student__usr_id = studentProfile.user.usr_id)
-    
-               
-    context =  { 'user': user,
-                'faculty':faculty,  
-                'StudentProfile': studentProfile
-                # 'StudentDetail': details
-                }
+    context =  { 'user': user,'faculty':faculty }
+    try:
+        student = StudentProfile.objects.get(user=user)
+        details=StudentDetails.objects.get(student_id=student.id)
+        hobbies=StudentHobbies.objects.get(student_id=student.id)
+        guardian=GuardianDetails.objects.get(student_id=student.id)
+        Medical=StudentMedicalReport.objects.get(student_id=student.id)
+        extraCurr=StudentExtraCurricular.objects.get(student_id=student.id)
+        achievements=[i for i in extraCurr.achievements.split(',')]
+        clubs=[i for i in extraCurr.clubs.split(',')]
+        hobbies=[i for i in hobbies.hobby.split(',')]
+        organizations=[i for i in extraCurr.organization.split(',')]
+        context.update({'student' : student,'details':details,'hobbies':hobbies,'guardian':guardian,'clubs':clubs,'hobbies':hobbies,'achievements':achievements,'orgs':organizations,'Medical':Medical})
+    except:
+        pass
     return render(request, 'FacultyDashboard/faculty-student-profile.html', context)
 
 @login_required
