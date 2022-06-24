372c9fcb8533d226251514da959ae337813b6e2e
Ambuj-Pandey <78492933+Ambuj-Pandey@users.noreply.github.com>
2022-06-25 00:19:40 +0530

    Added Google Calendar Fixes #30
    Added Google Calendar Fixes #30

---
+++ MPT/calender/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class BookingForm(forms.Form):
+    eventTitle = forms.CharField(label="event", max_length=255, required=True)
+    startDateTime = forms.DateTimeField(label="startDateTime", input_formats=['%Y/%m/%d %H:%M'], required=True)
+    endDateTime = forms.DateTimeField(label="endDateTime", input_formats=['%Y/%m/%d %H:%M'], required=True)
