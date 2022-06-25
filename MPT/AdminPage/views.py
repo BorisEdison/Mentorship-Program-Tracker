808bd00267e81a164cb22e6529ecff23e1dd412f
Bipin_Giri <bipingiri0085@gmail.com>
2022-06-25 23:32:11 +0530

    student edit for admin updated

--- MPT/AdminPage/views.py
+++ MPT/AdminPage/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserChangeForm
-from accounts.models import StudentProfile, User, MentorProfile, StudentDetails, StudentHobbies,GuardianDetails,StudentExtraCurricular,StudentMedicalReport
+from accounts.models import StudentProfile, User, MentorProfile, StudentDetails, GuardianDetails
 # from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required
 from EditUser.views import studentcontext
@@ -93,18 +93,11 @@ def updateuserprofile(request,usr_id):
         if not user.is_staff:
             profile = StudentProfile.objects.get(user__usr_id=usr_id)
             details,created=StudentDetails.objects.get_or_create(student_id=profile.id)
-            hobbies,created=StudentHobbies.objects.get_or_create(student_id=profile.id)
             guardian,created=GuardianDetails.objects.get_or_create(student_id=profile.id)
-            extraCurr,created=StudentExtraCurricular.objects.get_or_create(student_id=profile.id)
-            Medical,created=StudentMedicalReport.objects.get_or_create(student_id=profile.id)
             context.update({
                 'student': profile,
-                'pk': request.user.usr_id,
                 'details':details,
-                'hobbies':hobbies,
-                'guardian':guardian,
-                'extraCurr':extraCurr,
-                'Medical':Medical})
+                'guardian':guardian,})
             context.update(studentcontext)
             if request.method=='POST':
                 fName = request.POST['fName']
@@ -157,70 +150,6 @@ def updateuserprofile(request,usr_id):
                         details.YearOfAdmission = yearOfAdd
                 except:
                     pass
-
-                # guardian info
-                FatherName = request.POST['FatherName']
-                MotherName = request.POST['MotherName']
-                try:
-                    if request.POST['Fqualification']:
-                        Fqualif= request.POST['Fqualification']
-                        guardian.fatherHighestEducation=Fqualif
-                except:
-                    pass
-                try:
-                    if request.POST['Mqualification']:
-                        Mqualif= request.POST['Mqualification']
-                        guardian.motherHighestEducation=Mqualif
-                except:
-                    pass
-                try:
-                    if request.POST['Foccup']:
-                        Foccupation= request.POST['Foccup']
-                        guardian.fatherOccupation=Foccupation
-                except:
-                    pass
-                try:
-                    if request.POST['Moccup']:
-                        Moccupation= request.POST['Moccup']
-                        guardian.motherOccupation=Moccupation
-                except:
-                    pass
-                try:
-                    if request.POST['income']:
-                        income= request.POST['income']
-                        guardian.yearlyIncome=income
-                except:
-                    pass
-
-                guardian.father_name=FatherName
-                guardian.mother_name=MotherName
-
-                # health info
-                addiction= request.POST['addiction']
-                illness= request.POST['illness']
-                phobia= request.POST['phobia']
-                treatment= request.POST['treatment']
-                Medical.addiction=addiction
-                Medical.illness=illness
-                Medical.phobia=phobia
-                Medical.treatment=treatment
-                
-                #Goals, Hobbies, Extra Curricular Activities
-                aim= request.POST['aim']
-                hobbie= request.POST['hobby']
-                ReasonForEngg= request.POST['ReasonForEngg']
-                Achievements= request.POST['achievements']
-                clubs= request.POST['clubs']
-                orgs= request.POST['orgs']
-
-                details.reason_of_engg=ReasonForEngg
-                details.AimOfLife=aim
-                extraCurr.clubs=clubs
-                extraCurr.achievements=Achievements
-                extraCurr.organization=orgs
-                hobbies.hobby=hobbie
-
-                # user = User.objects.get(usr_id=stu_id)
                 try:
                     if 'profileImg' in request.FILES:
                         profile_img = request.FILES['profileImg']
@@ -242,10 +171,6 @@ def updateuserprofile(request,usr_id):
                 profile.mother_tongue = motherTongue
                 details.save()
                 profile.save()
-                guardian.save()
-                hobbies.save()
-                extraCurr.save()
-                Medical.save()
                 return redirect('admin-student')
             return render(request, 'AdminPage/admin-student-edit.html', context)
         
