58ce82cc2f587fd35768b5024d18f7d37052d41a
Boris Edison <borisedison@gmail.com>
2022-08-13 17:51:36 +0530

    basic new announcement page

--- MPT/Announcement/urls.py
+++ MPT/Announcement/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('facultyAnnoucement', views.facultyAnnouncement, name='faculty-announcement'),
+    path('facultyAnnoucement', views.facultyAnnouncement, name='faculty-announcement'),    
+    path('facultyAnnoucementNew', views.facultyAnnouncementNew, name='faculty-announcement-new'),
     path('studentAnnoucement', views.studentAnnouncement, name='student-announcement'),
 ]
--- MPT/Announcement/views.py
+++ MPT/Announcement/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def facultyAnnouncement(request):
     return render(request, 'Announcement/faculty-announcement.html' )
 
+def facultyAnnouncementNew(request):
+    return render(request, 'Announcement/faculty-announcement-new.html' )
+
 def studentAnnouncement(request):
     return render(request, 'Announcement/student-announcement.html' )
