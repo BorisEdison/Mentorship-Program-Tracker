4df6fcaf56b0eca11b52cf1aeef640a5996ca062
Boris Edison <borisedison@gmail.com>
2022-09-02 14:56:37 +0530

    minor changes

---
+++ MPT/MPT/static/js/student.js
@@ -0,0 +1,16 @@
+// ---------- Adding Datatables ------------------
+$(document).ready( function () {
+    $('#student-marks-table').DataTable({
+        //options
+    });
+  } );
+$(document).ready( function () {
+    $('#student-previous-table').DataTable({
+        //options
+    });
+} );
+$(document).ready( function () {
+    $('#student-upcoming-table').DataTable({
+        //options
+    });
+} );
