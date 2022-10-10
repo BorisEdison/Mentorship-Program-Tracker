b0b07ea72b754dc0328dcf03a2968f7a3a581932
Boris Edison <borisedison@gmail.com>
2022-10-10 10:30:59 +0530

    rendering DataTable in student dashboard

--- MPT/MPT/static/js/student.js
+++ MPT/MPT/static/js/student.js
@@ -1,22 +1,23 @@
 // ---------- Adding Datatables ------------------
-$(document).ready( function () {
-    $('#student-marks-table').DataTable({
+$(document).ready( () => {
+    $('#student-mark-table').DataTable({
         //options
-        dom: 'Blfrtip',
-        select:true,
-        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
-        dom: 'Bfrtip',
-        buttons: [
-        'copyHtml5',
-        'excelHtml5',
-        'csvHtml5',
-        'pdfHtml5',
-        'print',
-        'pageLength',
-        ]
+          dom: 'Blfrtip',
+          select:true,
+          "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
+          dom: 'Bfrtip',
+          buttons: [
+          'copyHtml5',
+          'excelHtml5',
+          'csvHtml5',
+          'pdfHtml5',
+          'print',
+          'pageLength',
+          ]
     });
   } );
-$(document).ready( function () {
+  
+$(document).ready( () => {
     $('#student-previous-table').DataTable({
         //options
         dom: 'Blfrtip',
@@ -33,7 +34,7 @@ $(document).ready( function () {
         ]
     });
 } );
-$(document).ready( function () {
+$(document).ready( () =>  {
     $('#student-upcoming-table').DataTable({
         //options
         dom: 'Blfrtip',
