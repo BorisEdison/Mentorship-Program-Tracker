b00b57f3b065f0a29c63379eea49b70d2a656cf3
BorisEdison <borisedison@gmail.com>
2022-12-03 14:59:50 +0530

    added LICENSE.txt

--- MPT/MPT/settings.py
+++ MPT/MPT/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j$#3ghp0o&1ez=(y(^g$gbsyb6b&a&o6k-cypz&0+tcv9!jha^'
+SECRET_KEY = os.environ.get('MPT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -163,7 +163,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS= True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mentorshipprogramtracker@gmail.com'
-EMAIL_HOST_PASSWORD = 'azviocifizqmdiha'
+EMAIL_HOST_USER = os.environ.get('MPT_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('MPT_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Testing <mentorshipprogramtracker@gmail.com>'
-
